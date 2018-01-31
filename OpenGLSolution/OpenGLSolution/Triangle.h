3b71acc496c84cb3bbeb989508aa866df006ef09
MartinClementson <martinc.91@hotmail.com>
2018-01-31 19:57:29 +0000

    Edit: Moved Code from Triangle class to the abstract mesh class

--- OpenGLSolution/OpenGLSolution/Triangle.h
+++ OpenGLSolution/OpenGLSolution/Triangle.h
@@ -1,38 +1,17 @@
 #pragma once
-#include <GL\glew.h>
-#include <GLFW\glfw3.h>
+#include "Mesh.h"
 
-struct Vertex
-{
-	GLfloat x, y, z;
-	GLfloat r, g, b;
-	Vertex() {};
-	Vertex(GLfloat x, GLfloat y, GLfloat z, GLfloat r, GLfloat g, GLfloat b)
-	{
-		this->x = x;
-		this->y = y;
-		this->z = z;
-		this->r = r;
-		this->g = g;
-		this->b = b;
-	}
-};
 
-class Triangle
+namespace GEO {
+
+class Triangle : public Mesh
 {
 public:
 	Triangle();
 	~Triangle();
 
 	void Draw();
-
-private:
-
-	Vertex vertices[3];
-	
-	
-	GLuint VBO, VAO;
-
-
 };
+
+} // End namespace GEO
 
